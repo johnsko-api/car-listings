ba7757f9d716ac1a7fd7ee43eafc2a37822d83d5
john ko <johnsko89@gmail.com>
2015-01-05 03:17:06 -0500

    about to fix crud functionality of manufacturer

--- car-listing/app/controllers/manufacturers_controller.rb
+++ car-listing/app/controllers/manufacturers_controller.rb
@@ -2,11 +2,37 @@ class ManufacturersController < ApplicationController
 
 
 def index
+  @manufacturers = Manufacturer.all
 end
 
 def new
   @manufacturer = Manufacturer.new
 end
+
+def show
+  @manufacturer = Manufacturer.find(params[:id])
+end
+
+def create
+  @manufacturer = Manufacturer.new(manufacturer_params)
+
+  if @manufacturer.save
+    flash[:notice] = "You've successfully submitted a manufacturer!"
+    redirect_to @manufacturer
+  else
+    flash[:alert] = "You have failed to meet the appropriate criteria"
+    render "new"
+  end
+end
+
+
+
+
+private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
+  end
 
 
 end
